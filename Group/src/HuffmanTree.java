0e68afc5658fefa69e8a4c90ecf9a94840bc5a7e
ohthatjeff <kushaannaskar@gmail.com>
2023-12-07 20:52:26 -0800

    Decompress (#2)

--- Group/src/HuffmanTree.java
+++ Group/src/HuffmanTree.java
@@ -9,18 +9,21 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class HuffmanTree implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static final char EOF = '\uFFFF';
     private Node root;
     private final File file;
     private Map<Character, Integer> charFrequencies = new HashMap<>();
     private Map<Character, String> huffmanCodes = new HashMap<>();
 
     /**
-    * frequency is the occurence of a character in list
+    * frequency is the occurrence of a character in list
     * parent nodes store the sum of frequency of children
     * @since 11-27-2023
     * @author Terry Ton, Esteban Madrigal , Manvir Hansra, Kushaan Naskar, Vinh Tran 
     */
-    public class Node implements Comparable<Node>{
+    public class Node implements Serializable, Comparable<Node>{
+        private static final long serialVersionUID = 1L;
         private Node left;
         private Node right;
         private int frequency;
@@ -51,7 +54,7 @@ public class HuffmanTree implements Serializable {
     }
 
     public class Leaf extends Node {
-    
+        private static final long serialVersionUID = 1L;
         private final char CHARACTER;
     
         public Leaf(char character, int frequency) {
@@ -86,6 +89,7 @@ public class HuffmanTree implements Serializable {
 
     private void encode() throws IOException {
         getCharFrequencies();
+        charFrequencies.put(EOF, 1);
         Queue<Node> queue = new PriorityQueue<>();
         charFrequencies.forEach((character, frequency) -> queue.add(new Leaf(character, frequency)));
         while (queue.size() > 1) {
@@ -93,6 +97,32 @@ public class HuffmanTree implements Serializable {
         }
         generateHuffmanCodes(root = queue.poll(), "");
     }
+    
+    /**
+     * Decodes the given encoded string.
+     * @param bits The encoded bits to be decoded.
+     * @return The decoded string.
+     */
+    public String decode(String bits) {
+        Node current = root;
+        StringBuilder decodeStr = new StringBuilder();
+
+        for (char bit : bits.toCharArray()) {
+        	if (bit == '0')
+        		current = current.left;
+        	else
+        		current = current.right;
+
+            if (current.left == null && current.right == null) {
+            	if (((Leaf) current).CHARACTER == EOF)
+                    break;  
+            	decodeStr.append(((Leaf)current).CHARACTER);
+                current = root;
+            }
+        }
+
+        return decodeStr.toString();
+    }     
 
     /**
      * 
