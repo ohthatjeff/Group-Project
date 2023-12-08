1044aa0403443d74bc1e2644e87bec4249b2a4fd
Kushaan Naskar <kushaannaskar@gmail.com>
2023-12-07 20:25:33 -0800

    Decompress

--- Group/src/DecompressFile.java
+++ Group/src/DecompressFile.java
@@ -1,6 +1,34 @@
-/**
- * Fill this file.
- */
-public class DecompressFile {
+import java.io.*;
 
+/**
+* This class implements Decompression given a compressed input file. 
+* @since 11-27-2023
+* @author Terry Ton, Esteban Madrigal , Manvir Hansra, Kushaan Naskar, Vinh Tran 
+*/
+public class DecompressFile {
+	
+	/**
+	 * Decompresses the given input file using Huffman coding and writes the decompressed data 
+	 * to the specified output file.
+	 *
+	 * @param in  Compressed file to be decompressed.
+	 * @param out Output file to write the decompressed data to.
+	 * @throws IOException Exception thrown if an error is detected during reading or writing of the file. 
+	 * @throws ClassNotFoundException Exception thrown if HuffmanTree class isn't found.
+	 */	
+    public static void decompress(File in, File out) throws IOException, ClassNotFoundException {
+        FileInputStream inputStream = new FileInputStream(in);
+        ObjectInputStream objInputStream = new ObjectInputStream(inputStream);
+        HuffmanTree huffmanTree = (HuffmanTree) objInputStream.readObject();
+        BitInputStream bitInputStream = new BitInputStream(inputStream);
+
+        String decodeStr = huffmanTree.decode(bitInputStream.readBits());
+
+        try (Writer writer = new OutputStreamWriter(new FileOutputStream(out))) {
+            writer.write(decodeStr);
+        }
+
+        objInputStream.close();
+        bitInputStream.close();
+    }
 }
--- Group/src/Driver.java
+++ Group/src/Driver.java
@@ -1,8 +1,18 @@
 import java.io.File;
 import java.io.IOException;
 
+/**
+* Driver to test Compression and Decompression using Huffman Coding. 
+* @since 11-27-2023
+* @author Terry Ton, Esteban Madrigal , Manvir Hansra, Kushaan Naskar, Vinh Tran 
+*/
 public class Driver {
     public static void main(String[] args) throws IOException {
-        CompressFile.compress(new File("input.txt"), new File("output.txt"));
+        CompressFile.compress(new File("input.txt"), new File("output.txt"));        
+        try {
+            DecompressFile.decompress(new File("output.txt"), new File("decompressedOutput.txt"));
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }        
     }
 }
